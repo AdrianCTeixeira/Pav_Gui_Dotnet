0ac016452f93f922d1048260753c6160a5bfb72c
Adrian Campos Teixeira <adrian.teixeira@c2es.com.br>
2018-12-01 18:40:19 -0200

    Adicionado processamento com Threads. Adicionado divisao de Threads.

--- Interface/Core/Database.cs
+++ Interface/Core/Database.cs
@@ -9,6 +9,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Core
@@ -18,13 +19,43 @@ namespace Core
         private int Cont = 0;
         public void SalvarLista(List<MapSummary> summarie, List<MapSummaryN> summarieN)
         {
-            using (SqlConnection connection = new SqlConnection("Server=DESKTOP-SISBMK5;Database=myDataBase;Trusted_Connection=True;"))
+
+            int i = 0;
+            var list = summarie;
+            var list2 = summarieN;
+            int threadNumbers = 4;
+            int eachThread = summarie.Count / (threadNumbers);
+            Thread thread = new Thread(() => NewMethod(summarie, i, 0, (eachThread * 1)));
+            Thread thread2 = new Thread(() => NewMethod(summarie, i, eachThread * 1, eachThread * 2));
+            Thread thread3 = new Thread(() => NewMethod(summarie, i, eachThread * 2, eachThread * 3));
+            Thread thread4 = new Thread(() => NewMethod(summarie, i, eachThread * 3, eachThread * 4));
+
+            thread.Start();
+            thread2.Start();
+            thread3.Start();
+            thread4.Start();
+
+            //foreach (Object ob in list)
+            //{
+            //    var item = list2.FirstOrDefault(o => o.MarketName == list[i].MarketName);
+            //    metroGrid1.Rows.Add(list[i].MarketCurrency, list[i].BaseCurrency, list[i].MarketCurrencyLong, list[i].BaseCurrencyLong,
+            //list[i].MinTradeSize, list[i].MarketName, item.Ask, list[i].IsActive, list[i].IsRestricted, list[i].Created, list[i].Notice, list[i].IsSponsored, list[i].LogoUrl);
+            //    i++;
+            //}
+
+        }
+
+        private static int NewMethod(List<MapSummary> summarie, int i, int iniciarEm, int pararEm)
+        {
+            using (SqlConnection conn = new SqlConnection("Server=DESKTOP-SISBMK5;Database=api;Integrated Security=true"))
             {
-                connection.Open();
-                int i = 0;
-                var list = summarie;
-                var list2 = summarieN;
-                foreach (MapSummary objeto in summarie)
+                conn.Open();
+                List<MapSummary> summarie2 = new List<MapSummary>();
+                for (int x = iniciarEm; x < pararEm; x++)
+                {
+                    summarie2.Add(summarie[x]);
+                }
+                foreach (MapSummary objeto in summarie2)
                 {
                     byte[] data = null;
                     using (WebClient webClient = new WebClient())
@@ -48,28 +79,20 @@ namespace Core
                         ")";
                     using (SqlCommand querySaveStaff = new SqlCommand(saveStaff))
                     {
-                        querySaveStaff.Connection = connection;
+                        querySaveStaff.Connection = conn;
                         querySaveStaff.CommandType = CommandType.Text;
                         querySaveStaff.CommandText = saveStaff;
                         querySaveStaff.ExecuteNonQuery();
                     }
-                    Console.WriteLine(i);
+                    int contPositionList = iniciarEm;
+                    Console.WriteLine(contPositionList);
+                    contPositionList++;
                     i++;
-                    System.Threading.Thread.Sleep(100);
-                    return;
                 }
-                connection.Close();
-
-                Console.WriteLine("");
-                //foreach (Object ob in list)
-                //{
-                //    var item = list2.FirstOrDefault(o => o.MarketName == list[i].MarketName);
-                //    metroGrid1.Rows.Add(list[i].MarketCurrency, list[i].BaseCurrency, list[i].MarketCurrencyLong, list[i].BaseCurrencyLong,
-                //list[i].MinTradeSize, list[i].MarketName, item.Ask, list[i].IsActive, list[i].IsRestricted, list[i].Created, list[i].Notice, list[i].IsSponsored, list[i].LogoUrl);
-                //    i++;
-                //}
+                return i;
             }
         }
+
         public int getCont()
         {
             return Cont;
